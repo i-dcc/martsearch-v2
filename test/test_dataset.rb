6640604af0128a0a1442f7b1e0827fc466b50798
Daz Oakley <daz.oakley@gmail.com>
2009-10-20 21:16:52 +0100

    Fleshing out the test suites...

--- test/test_dataset.rb
+++ test/test_dataset.rb
@@ -2,33 +2,23 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class DatasetTest < Test::Unit::TestCase
   
-  @http_client = Net::HTTP
-  if ENV['http_proxy']
-    proxy = URI.parse( ENV['http_proxy'] )
-    @http_client = Net::HTTP::Proxy( proxy.host, proxy.port )
-  end
-  
-  config_file = File.new( File.dirname(__FILE__) + "/../config/config.json", "r" )
-  @config     = JSON.load(config_file)
-  
-  @index = Index.new( @config["index"], @http_client )
-  
-  @datasets = []
-  @config["datasets"].each do |ds|
-    ds_conf_file = File.new( File.dirname(__FILE__) + "/../config/datasets/#{ds}/config.json","r")
-    ds_conf      = JSON.load(ds_conf_file)
-    @datasets.push( Dataset.new( ds_conf, @http_client ) )
-  end
-  
-  @datasets.each do |dataset|
+  @@datasets.each do |dataset|
     context "Dataset '#{dataset.display_name}'" do
+      
+      should "have basic attributes" do
+        assert( dataset.url, ".url is nil - incorrect parsing of config?" )
+        assert( dataset.display_name, ".display_name is nil - incorrect parsing of config?" )
+      end
       
       should "respond to pings" do
         assert( dataset.is_alive?, "The biomart dataset #{dataset.dataset_name} is offline or misconfigured." )
       end
       
       should "fail gracefully when we mess with the url" do
-        
+        orig_url   = dataset.url
+        dataset.url = "http://www.foo.com"
+        assert_equal( dataset.is_alive?, false, "The .is_alive? function does not correctly report a broken dataset." )
+        dataset.url = orig_url
       end
       
       should "correctly handle a simple (single item) search" do
@@ -36,6 +26,10 @@ class DatasetTest < Test::Unit::TestCase
       end
       
       should "correctly handle a more complicated (large) search" do
+        
+      end
+      
+      should "generate a biomart search link url" do
         
       end
       
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,3 +15,24 @@ Dir[ File.dirname(__FILE__) + '/../lib/*.rb' ].each do |file|
   require file
 end
 
+##
+## Some basic setup shared between the test suites
+##
+
+@@http_client = Net::HTTP
+if ENV['http_proxy']
+  proxy = URI.parse( ENV['http_proxy'] )
+  @@http_client = Net::HTTP::Proxy( proxy.host, proxy.port )
+end
+
+config_file = File.new( File.dirname(__FILE__) + "/../config/config.json", "r" )
+@@config = JSON.load(config_file)
+
+@@index = Index.new( @@config["index"], @@http_client )
+
+@@datasets = []
+@@config["datasets"].each do |ds|
+  ds_conf_file = File.new( File.dirname(__FILE__) + "/../config/datasets/#{ds}/config.json","r")
+  ds_conf      = JSON.load(ds_conf_file)
+  @@datasets.push( Dataset.new( ds_conf, @@http_client ) )
+end

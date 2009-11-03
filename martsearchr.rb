9253471862d0347c9c58ed465144be6882d261ee
Daz Oakley <daz.oakley@gmail.com>
2009-11-03 15:01:30 +0000

    Slight refactor of where we store the dataset template files...

--- martsearchr.rb
+++ martsearchr.rb
@@ -25,10 +25,19 @@ end
 helpers do
   include WillPaginate::ViewHelpers
   
-  def dataset_template( template_file, result_data )
-    template = File.open( "#{Dir.pwd}/config/datasets/#{template_file}", "r" )
-    erb      = ERB.new( template.read )
-    return erb.result( binding )
+  def partial(template, *args)
+    template_array = template.to_s.split('/')
+    template = template_array[0..-2].join('/') + "/_#{template_array[-1]}"
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    options.merge!(:layout => false)
+    if collection = options.delete(:collection) then
+      collection.inject([]) do |buffer, member|
+        buffer << erb(:"#{template}", options.merge(:layout =>
+        false, :locals => {template_array[-1].to_sym => member}))
+      end.join("\n")
+    else
+      erb(:"#{template}", options)
+    end
   end
   
   def tag_options(options, escape = true)

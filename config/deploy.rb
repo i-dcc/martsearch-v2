1846a773467d2cf8a68eb9613a633fb2adbe4b56
Daz Oakley <daz.oakley@gmail.com>
2009-12-16 11:30:40 +0000

    Updated the main page to point to the new heatmap.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,6 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/log #{release_path}/log"
     run "ln -nfs #{shared_path}/cache #{release_path}/tmp/cache"
     run "ln -nfs #{release_path}/public #{shared_path}/htdocs/mouseportal"
-    run "ln -nfs #{shared_path}/pheno_overview.xls #{release_path}/public/pheno_overview.xls"
     run "ln -nfs #{shared_path}/pheno_images #{release_path}/public/images/pheno_images"
     run "ln -nfs #{shared_path}/pheno_abr #{release_path}/tmp/pheno_abr"
   end

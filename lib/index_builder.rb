67d9907993487a8fc2554f05231e3725a69a5d14
Daz Oakley <daz.oakley@gmail.com>
2010-01-05 16:37:32 +0000

    Built in some index document creation code ported over (and cleaned up) from the original MartSearch.

--- lib/index_builder.rb
+++ lib/index_builder.rb
@@ -1,7 +1,8 @@
 # This class is responsible for the set-up, building and updating 
 # of a Solr search index for use with a MartSearchr application.
 class IndexBuilder
-  attr_reader :martsearch, :index_conf
+  attr_reader :martsearch, :index_conf, :documents, :documents_by
+  attr_accessor :test_environment
   
   def initialize( config_desc )
     @http_client = Net::HTTP
@@ -10,10 +11,15 @@ class IndexBuilder
       @http_client = Net::HTTP::Proxy( proxy.host, proxy.port )
     end
     
-    @martsearch = Martsearch.new(config_desc)
-    
-    @index_conf = @martsearch.config["index"]
+    @test_environment       = false
+    @martsearch             = Martsearch.new(config_desc)
+    @index_conf             = @martsearch.config["index"]
     @index_conf["datasets"] = dataset_index_conf()
+    
+    # Create a placeholder variable to store docs in (and a cache variable 
+    # for faster lookups if required...)
+    @documents    = {}
+    @documents_by = {}
   end
   
   # Function to create the Solr XML Schema used to define 
@@ -25,12 +31,54 @@ class IndexBuilder
     return schema
   end
   
-  
+  # Function to build the documents array for pushing into our index.  
+  # Does this by pulling all of the available data from each dataset 
+  # and processing each returned result according to the "indexing" 
+  # config defined on a per-dataset basis.
+  def build_documents
+    @index_conf["datasets"].each do |dataset_conf|
+      # Only index a dataset if we have been asked to...
+      if dataset_conf["index"]
+        unless @test_environment
+          puts "Fetching data from dataset: '#{dataset_conf["display_name"]}'"
+        end
+        
+        # Extract all of the needed index mapping data from "attribute_map"
+        mapping_data = process_attribute_map( dataset_conf["indexing"]["attribute_map"] )
+        
+        attribute_map      = mapping_data[:attribute_map]
+        primary_attribute  = mapping_data[:primary_attribute]
+        map_to_index_field = mapping_data[:map_to_index_field]
+        
+        # Do we need to cache lookup data?
+        unless map_to_index_field == @index_conf["schema"]["unique_key"]
+          cache_documents_by( map_to_index_field )
+        end
+        
+        # Grab a Biomart::Dataset object and search and retrieve all the data it holds
+        mart    = biomart_dataset( dataset_conf )
+        results = mart.search( :attributes => attribute_map.keys )
+        
+        # Now loop through the results building up document structures
+        puts "Processing #{results[:data].size} rows of Biomart results"
+        process_dataset_results( dataset_conf, results, attribute_map, map_to_index_field, primary_attribute )
+      end
+    end
+    
+    # Finally, remove duplicates from our documents
+    @documents.values.each do |doc|
+      clean_document(doc)
+    end
+  end
   
   private
   
+  ##
+  ## General Utility functions
+  ##
+  
   # Utility function to grab the index configuration from each of 
-  # the datasets to be added into our @index_conf
+  # the datasets to be added into our @index_conf.
   def dataset_index_conf
     dataset_conf = []
     @martsearch.datasets.each do |ds|
@@ -38,4 +86,210 @@ class IndexBuilder
     end
     return dataset_conf
   end
+  
+  # Utility function to either find or create a Biomart::Dataset object
+  def biomart_dataset( ds_conf )
+    if @martsearch.datasets_by_name[ ds_conf["dataset_name"].to_sym ].dataset
+      return @martsearch.datasets_by_name[ ds_conf["dataset_name"].to_sym ].dataset
+    else
+      return Biomart::Dataset.new( ds_conf["url"], { :name => ds_conf["dataset_name"] } )
+    end
+  end
+  
+  ##
+  ## Utility functions specific for building the XML to prime a 
+  ## Solr index
+  ##
+  
+  # Utility function to process the attribute_map configuration into 
+  # something we can use to map biomart results to our index configuration.
+  def process_attribute_map( attribute_map )
+    map                = {}
+    primary_attribute  = nil
+    map_to_index_field = nil
+
+    # Extract all of the needed index mapping data from the "attribute_map"
+    # - The "attribute_map" defines how the biomart attributes relate to the index "fields"
+    # - The "primary_attribute" is the biomart attribute used to associate a set of biomart 
+    #   results to an index "doc" - using the "map_to_index_field" field as the link.
+    attribute_map.each do |mapping_obj|
+      if mapping_obj["use_to_map"]
+        if primary_attribute
+          raise StandardError "You have defined more than one attribute to map to the index with! Please check your config..."
+        else
+          primary_attribute  = mapping_obj["attr"]
+          map_to_index_field = mapping_obj["idx"]
+        end
+      end
+      map[ mapping_obj["attr"] ] = mapping_obj
+    end
+    
+    unless primary_attribute
+      raise StandardError "You have not specified an attribute to map to the index with in #{dataset["dataset_name"]}!"
+    end
+    
+    return {
+      :attribute_map      => map,
+      :primary_attribute  => primary_attribute,
+      :map_to_index_field => map_to_index_field
+    }
+  end
+  
+  # Utility function to create a new document construct.
+  def new_document()
+    # Work out fields to ignore - these will be auto populated by Solr
+    copy_fields = []
+    @index_conf["schema"]["copy_fields"].each do |copy_field|
+      copy_fields.push( copy_field["dest"] )
+    end
+    
+    doc = {}
+    @index_conf["schema"]["fields"].each do |key,detail|
+      unless copy_fields.include?(key)
+        doc[key] = []
+      end
+    end
+    return doc
+  end
+  
+  # Utility function to find a specific document (i.e. for a gene), arguments 
+  # are the field to search on, and the term to find.
+  def find_document( field, search_term )
+    if field == @index_conf["schema"]["unique_key"]
+      return @documents[search_term]
+    else
+      if @documents_by[field][search_term]
+        return @documents[ @documents_by[field][search_term] ]
+      else
+        return nil
+      end
+    end
+  end
+  
+  # Utility function to cache the document store by a given field.  This 
+  # allows a much faster lookup of documents when we are not linking by 
+  # the primary field.
+  def cache_documents_by( field )
+    # Test to see if we really need to build the cache - it could have 
+    # already been done by a previous dataset...
+    build_cache = true
+    if @documents_by[field] and ( @documents.keys.size === @documents_by[field].keys.size )
+      build_cache = false
+    end
+    
+    if build_cache
+      unless @test_environment
+        puts "caching documents by '#{field}'"
+      end
+      @documents_by[field] = {}
+      @documents.each do |key,values|
+        values[field].each do |value|
+          @documents_by[field][value] = key
+        end
+      end
+    end
+  end
+  
+  # Utility function to run through all of the results returned from 
+  # the dataset and either create document constructs or append data to 
+  # existing document constructs.
+  def process_dataset_results( dataset_conf, results, attribute_map, map_to_index_field, primary_attribute )
+    # Figure out the position of the primary_attribute in the results array
+    primary_attribute_pos = nil
+    results[:headers].each_index do |position|
+      if results[:headers][position] == primary_attribute
+        primary_attribute_pos = position
+      end
+    end
+    
+    # Now loop through the result data...
+    results[:data].each do |data_row|
+      # First check we have something to map back to the index with - if not, move along...
+      if data_row[primary_attribute_pos]
+        # Find us a doc object to map to...
+        doc = find_document( map_to_index_field, data_row[primary_attribute_pos] )
+        
+        # If we can't find one - see if we're allowed to create one
+        unless doc
+          if dataset_conf["indexing"]["allow_document_creation"]
+            @documents[ data_row[primary_attribute_pos] ] = new_document()
+            doc = @documents[ data_row[primary_attribute_pos] ]
+          end
+        end
+        
+        # Okay, if we have a doc - process the biomart attributes
+        if doc
+          # First, create a hash out of the data_row
+          data_obj = {}
+          results[:headers].each_index do |position|
+            data_obj[ results[:headers][position] ] = data_row[position]
+          end
+          
+          # Now do the processing
+          data_obj.each do |attr_name,attr_value|
+            # Extract and index our initial data return
+            value_to_index = extract_value_to_index( attr_name, attr_value, attribute_map, data_obj )
+            if value_to_index && doc[ attribute_map[attr_name]["idx"] ]
+              doc[ attribute_map[attr_name]["idx"] ].push( value_to_index )
+            end
+            
+            # Any further metadata to be extracted from here? (i.e. MP terms in comments)
+            if value_to_index and attribute_map[attr_name]["extract"]
+              regexp = Regexp.new( attribute_map[attr_name]["extract"]["regexp"] )
+              matches = regexp.match( value_to_index )
+              if matches
+                doc[ attribute_map[attr_name]["extract"]["idx"] ].push( matches[0] )
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  # Utility function to determine what data values we need to 
+  # add to the index given the dataset configuration.
+  def extract_value_to_index( attr_name, attr_value, attr_options, mart_data )
+    options        = attr_options[attr_name]
+    value_to_index = attr_value
+
+    if options["if_attr_equals"]
+      unless options["if_attr_equals"].include?( attr_value )
+        value_to_index = nil
+      end
+    end
+
+    if options["index_attr_name"]
+      if value_to_index
+        value_to_index = attr_name
+      end
+    end
+
+    if options["if_other_attr_indexed"]
+      other_attr       = options["if_other_attr_indexed"]
+      other_attr_value = mart_data[ other_attr ]
+
+      unless extract_value_to_index( other_attr, other_attr_value, attr_options, mart_data )
+        value_to_index = nil
+      end
+    end
+
+    return value_to_index
+  end
+  
+  # Utility function to remove any duplication from a document construct.
+  def clean_document( doc )
+    doc.each do |key,value|
+      if value.size > 0
+        doc[key] = value.uniq
+      end
+    end
+  end
+  
+  ##
+  ## Utility functions for creating and handling XML files
+  ##
+  
+  
+  
 end

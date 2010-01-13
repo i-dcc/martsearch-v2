b8c04259966ed1bfcb618067ddd62943b577c035
Daz Oakley <daz.oakley@gmail.com>
2010-01-13 13:52:14 +0000

    Fixed a merge cock-up...

--- config/datasets/ikmc-dcc-gene_details/custom_sort.rb
+++ config/datasets/ikmc-dcc-gene_details/custom_sort.rb
@@ -7,7 +7,7 @@ sorted_results = {}
       "synonyms"         => [],
       "ensembl_gene_ids" => [],
       "vega_gene_ids"    => [],
-      "ncbi_gene_ids"    => [],
+      "entrez_gene_ids"  => [],
       "ccds_ids"         => [],
       "omim_ids"         => []
     }
@@ -22,12 +22,12 @@ sorted_results = {}
   result_data["end"]              = result["end"]
   result_data["strand"]           = result["strand"]
 
-  result_data["marker_names"].push( result["marker_name"] )         if result["marker_name"]
-  result_data["synonyms"].push( result["synonym"] )                 if result["synonym"]
-  result_data["ensembl_gene_ids"].push( result["ensembl_gene_id"] ) if result["ensembl_gene_id"]
-  result_data["vega_gene_ids"].push( result["vega_gene_id"] )       if result["vega_gene_id"]
-  result_data["entrez_gene_ids"].push( result["ncbi_gene_id"] )     if result["ncbi_gene_id"]
-  result_data["ccds_ids"].push( result["ccds_id"] )                 if result["ccds_id"]
+  result_data["marker_names"].push( result["marker_name"] )
+  result_data["synonyms"].push( result["synonym"] )
+  result_data["ensembl_gene_ids"].push( result["ensembl_gene_id"] )
+  result_data["vega_gene_ids"].push( result["vega_gene_id"] )
+  result_data["entrez_gene_ids"].push( result["ncbi_gene_id"] )
+  result_data["ccds_ids"].push( result["ccds_id"] )
   
   if result["omim_id"] and result["omim_description"]
     result_data["omim_ids"].push({ "id" => result["omim_id"], "desc" => result["omim_description"] })
@@ -40,7 +40,7 @@ sorted_results.each do |key,result_data|
   result_data["synonyms"].uniq!
   result_data["ensembl_gene_ids"].uniq!
   result_data["vega_gene_ids"].uniq!
-  result_data["ncbi_gene_ids"].uniq!
+  result_data["entrez_gene_ids"].uniq!
   result_data["ccds_ids"].uniq!
   result_data["omim_ids"].uniq!
 end

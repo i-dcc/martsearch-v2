9b5956275077a4775e0848e5b43a057a79c5791f
Daz Oakley <daz.oakley@gmail.com>
2010-01-13 14:05:53 +0000

    Added back some logic that was removed by mistake.

--- config/datasets/ikmc-dcc-gene_details/custom_sort.rb
+++ config/datasets/ikmc-dcc-gene_details/custom_sort.rb
@@ -21,13 +21,13 @@ sorted_results = {}
   result_data["start"]            = result["start"]
   result_data["end"]              = result["end"]
   result_data["strand"]           = result["strand"]
-
-  result_data["marker_names"].push( result["marker_name"] )
-  result_data["synonyms"].push( result["synonym"] )
-  result_data["ensembl_gene_ids"].push( result["ensembl_gene_id"] )
-  result_data["vega_gene_ids"].push( result["vega_gene_id"] )
-  result_data["entrez_gene_ids"].push( result["ncbi_gene_id"] )
-  result_data["ccds_ids"].push( result["ccds_id"] )
+  
+  result_data["marker_names"].push( result["marker_name"] )         if result["marker_name"]
+  result_data["synonyms"].push( result["synonym"] )                 if result["synonym"]
+  result_data["ensembl_gene_ids"].push( result["ensembl_gene_id"] ) if result["ensembl_gene_id"]
+  result_data["vega_gene_ids"].push( result["vega_gene_id"] )       if result["vega_gene_id"]
+  result_data["entrez_gene_ids"].push( result["ncbi_gene_id"] )     if result["ncbi_gene_id"]
+  result_data["ccds_ids"].push( result["ccds_id"] )                 if result["ccds_id"]
   
   if result["omim_id"] and result["omim_description"]
     result_data["omim_ids"].push({ "id" => result["omim_id"], "desc" => result["omim_description"] })

26ffca344f655dd975a87a34a6acd46c4c1de036
Daz Oakley <daz.oakley@gmail.com>
2010-03-23 20:53:42 +0000

    Added in a jQuery plugin for font resizing. [Closes #76]

--- martsearchr.rb
+++ martsearchr.rb
@@ -27,7 +27,7 @@ require "#{MARTSEARCHR_PATH}/lib/martsearch.rb"
 # We're going to use the version number as a cache breaker 
 # for the CSS and javascript code. Update with each release 
 # of your portal (especially if you change the CSS or JS)!!!
-PORTAL_VERSION = "0.0.7"
+PORTAL_VERSION = "0.0.8"
 
 # Initialise the MartSearch object
 @@ms = Martsearch.new( "#{MARTSEARCHR_PATH}/config/config.json" )
@@ -301,6 +301,7 @@ get "/css/martsearch*.css" do
     "reset.css",
     "jquery.prettyPhoto.css",
     "jquery.tablesorter.css",
+    "jquery.fontresize.css",
     "jquery-ui-1.7.2.redmond.css",
     "screen.css"
   ]

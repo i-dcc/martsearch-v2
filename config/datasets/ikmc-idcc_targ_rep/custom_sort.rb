812cc5423f3cb4268ad2f329bc7a55baac6da2fb
Sebastien Briois <sb25@mib18858i.dynamic.sanger.ac.uk>
2010-04-07 11:40:28 +0100

    All projects for each pipeline are available but only the most advanced one is shown. Knockout attempts templates removed.

--- config/datasets/ikmc-idcc_targ_rep/custom_sort.rb
+++ config/datasets/ikmc-idcc_targ_rep/custom_sort.rb
@@ -9,22 +9,43 @@ sorted_results = {}
   result_data = sorted_results[ result[ @joined_biomart_attribute ] ]
   
   unless result_data[ result['pipeline_name'] ]
-    result_data[ result['pipeline_name'] ] = {
+    result_data[ result['pipeline_name'] ] = {}
+  end
+  
+  pipeline = result_data[ result['pipeline_name'] ]
+  key = [
+    result['homology_arm_start'],
+    result['homology_arm_end'],
+    result['cassette_start'],
+    result['cassette_end'],
+    result['cassette'],
+    result['backbone']
+  ]
+  unless pipeline[ key ]
+    pipeline[ key ] = {
       'molecular_structure_id'  => result['molecular_structure_id'],
       'pipeline_name'           => result['pipeline_name'],
       'mgi_accession_id'        => result['mgi_accession_id'],
       'design_id'               => result['design_id'],
-      'design_type'             => result['design_type'],
-      'ikmc_project_id'         => result['ikmc_project_id'],
       'targeting_vectors'       => [],
       'conditional_clones'      => [],
       'nonconditional_clones'   => [],
       'vector_available'        => false,
       'escell_available'        => false,
-      'mouse_available'         => false
+      'mouse_available'         => false,
+      'display'                 => false
     }
   end
-  project = result_data[ result['pipeline_name'] ]
+  
+  project = pipeline[ key ]
+  
+  # Get ikmc_project_id
+  unless result['ikmc_project_id'].nil? or result['ikmc_project_id'].empty?
+    project['ikmc_project_id'] = result['ikmc_project_id']
+    ikmc_project_id = result['ikmc_project_id']
+  else
+    ikmc_project_id = project['ikmc_project_id']
+  end
   
   #
   #   Targeting Vector
@@ -32,7 +53,7 @@ sorted_results = {}
   project['vector_available'] = true
   
   targ_vec = {
-    'ikmc_project_id'     => result['ikmc_project_id'],
+    'ikmc_project_id'     => ikmc_project_id,
     'cassette'            => result['cassette'],
     'backbone'            => result['backbone'],
     'targeting_vector'    => result['targeting_vector'],
@@ -49,16 +70,15 @@ sorted_results = {}
   next unless result['escell_clone']
   project['escell_available'] = true
   
-  # This project has cells, it might be more interesting than 
-  # the one set initially.
-  project['ikmc_project_id'] = result['ikmc_project_id']
-  
   es_cell = {
+    'ikmc_project_id'           => ikmc_project_id,
+    'targeting_vector'          => result['targeting_vector'],
     'escell_clone'              => result['escell_clone'],
     'allele_symbol_superscript' => result['allele_symbol_superscript'],
     'parental_cell_line'        => result['parental_cell_line'],
   }
   
+  # Push ES Cell clone to the right Array (``conditional`` or ``nonconditional``)
   if result['design_type'] == 'Knock Out' and result['loxp_start'].nil?
     clone_type = "nonconditional_clones"
   else
--- config/datasets/ikmc-idcc_targ_rep/view_helpers.rb
+++ config/datasets/ikmc-idcc_targ_rep/view_helpers.rb
@@ -1,25 +1,29 @@
 
-def idcc_targ_rep_get_progressbar_info( mol_struct )
-  if mol_struct['mouse_available']
+def idcc_targ_rep_get_progressbar_info( project )
+  if project['mouse_available']
     return { "vectors" => "normal", "cells" => "normal", "mice" => "normal" }
-  elsif mol_struct['escell_available']
-    return { "vectors" => "normal", "cells" => "normal", "mice" => "incomp" }
-  elsif mol_struct['vector_available']
-    return { "vectors" => "normal", "cells" => "incomp", "mice" => "incomp" }
-  else
-    # Nothing available - should not happen.
-    return { "vectors" => "incomp", "cells" => "incomp", "mice" => "incomp" }
   end
+  
+  if project['escell_available']
+    return { "vectors" => "normal", "cells" => "normal", "mice" => "incomp" }
+  end
+  
+  if project['vector_available']
+    return { "vectors" => "normal", "cells" => "incomp", "mice" => "incomp" }
+  end
+  
+  # Nothing available - should not happen.
+  return { "vectors" => "incomp", "cells" => "incomp", "mice" => "incomp" }
 end
 
 # Link URL generator for ordering products.
-def idcc_targ_rep_product_order_url( mol_struct, result_data, order_type )
+def idcc_targ_rep_product_order_url( project, result_data, order_type )
   url = ""
   
-  pipeline_name     = mol_struct['pipeline_name']
-  mgi_accession_id  = mol_struct['mgi_accession_id'][4..-1]
-  project_id        = mol_struct['ikmc_project_id']
-  marker_symbol     = result_data["index"]["marker_symbol"]
+  pipeline_name     = project['pipeline_name']
+  mgi_accession_id  = project['mgi_accession_id'][4..-1]
+  project_id        = project['ikmc_project_id']
+  marker_symbol     = result_data['index']['marker_symbol']
   
   if pipeline_name === "KOMP-CSD"
     case order_type
@@ -41,7 +45,7 @@ def idcc_targ_rep_product_order_url( mol_struct, result_data, order_type )
       url = "http://www.komp.org/geneinfo.php?project=#{project_id}"
     end
   
-  elsif ( pipeline_name === "EUCOMM" or pipeline_name === "mirKO" )
+  elsif pipeline_name === "EUCOMM"
     case order_type
     when "vectors"
       url = "http://www.eummcr.org/final_vectors.php?mgi_id=#{mgi_accession_id}"

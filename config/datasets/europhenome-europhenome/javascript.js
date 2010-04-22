d76b45204499c51e9dd2895da58aec11e49323e2
Daz Oakley <daz.oakley@gmail.com>
2010-04-22 10:25:03 +0100

    Cleaned up the two phenotyping datasets and made them look a little more consistent.

--- config/datasets/europhenome-europhenome/javascript.js
+++ config/datasets/europhenome-europhenome/javascript.js
@@ -7,9 +7,7 @@ jQuery(document).ready(function() {
     jQuery(this).attr( "title", "" );
     jQuery(this).qtip({
        content:  jQuery(this).attr("tooltip"),
-       // show:     { when: { event: "mouseover" }, solo: true },
-       // hide:     { when: { event: "unfocus" } },
-       style:    { tip: "topMiddle", border: { radius: 5 }, width: 400, name: "dark" },
+       style:    { tip: "topMiddle", border: { radius: 5 }, width: 400, name: "light" },
        position: { corner: { target: "bottomMiddle", tooltip: "topMiddle" } }
     });
   });

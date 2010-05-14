0664b928ab18bdb90f4ede6c55ce617deb71d2af
Sebastien Briois <sb25@guest165.wtgc.org>
2010-05-14 11:09:04 +0100

    More stable project report (less sensitive to missing data)

--- project_report.rb
+++ project_report.rb
@@ -1,43 +1,41 @@
 get "/project/:id" do
+  project_id  = params[:id]
+  
   @current    = "home"
-  @page_title = "Report for project #{params[:id]}"
-  @data       = get_data( params[:id] )
+  @page_title = "Report for project #{project_id}"
+  @data = { 'project_id' => project_id }
+  @data.update( get_common_data(project_id) )
+  @data.update( get_vectors_and_cells(project_id) )
+  @data.update( get_mice(@data['ensembl_gene_id']) ) if @data['ensembl_gene_id']
+  @data.update( order_buttons_url(@data) )
   
   erubis :project_report
 end
 
-def get_data( project_id )
-  data = { :project_id => project_id }
+# Will query DCC gene details mart
+def get_common_data( project_id )
+  conf    = JSON.load( File.new("#{File.dirname(__FILE__)}/config/datasets/ikmc-dcc-gene_details/config.json","r") )
+  dataset = Biomart::Dataset.new( conf['url'], { :name => conf['dataset_name'] } )
+  results = dataset.search({
+    :filters    => { 'ikmc_project_id' => project_id },
+    :attributes => [ 'marker_symbol', 'mgi_accession_id', 'ensembl_gene_id', 
+      'vega_gene_id', 'ikmc_project', 'status', 
+      'mouse_available', 'escell_available', 'vector_available' ],
+    :process_results => true
+  })
   
-  query_index( data, project_id ) # To get common data
-  
-  query_idcc_targ_rep_mart( data, project_id )
-  query_ko_attempts_mart( data, project_id )
-  query_kermits( data, project_id )
-  
-  return data
+  return results[0] if results
+  return {}
 end
 
-def query_index( data, project_id )
-  results = @@ms.index.search( query = "ikmc_project_id:#{project_id}")
-  index_results = results.values()[0]['index']
-
-  data_wanted = ['colony_prefix', 'allele', 'product_status', 'vega_gene_id',
-    'marker_symbol', 'mgi_accession_id', 'ensembl_gene_id'
-  ]
-  returned_hash = {}
-  data_wanted.inject({}){|returned_hash, key| returned_hash.update( key.to_sym => index_results[key] )}
-  
-  data.update( returned_hash )
-end
-
-def query_idcc_targ_rep_mart( data, project_id )
+# Will query IDCC targ rep mart
+def get_vectors_and_cells( project_id )
   conf    = JSON.load( File.new("#{File.dirname(__FILE__)}/config/datasets/ikmc-idcc_targ_rep/config.json","r") )
   dataset = Biomart::Dataset.new( conf['url'], { :name => conf['dataset_name'] } )
   results = dataset.search({
     :filters => { 'ikmc_project_id' => project_id },
     :attributes => [
-      'allele_id', 'pipeline', 'design_id', 'mutation_subtype',
+      'allele_id', 'design_id', 'mutation_subtype',
       'cassette', 'backbone', 'targeting_vector', 'intermediate_vector',
       'allele_symbol_superscript', 'escell_clone', 'parental_cell_line',
       'floxed_start_exon'
@@ -45,95 +43,138 @@ def query_idcc_targ_rep_mart( data, project_id )
     :process_results => true
   })
   
+  data = {}
+  
   results.each do |result|
-    if not data.keys.include? :pipeline
-      data.update({
-        :pipeline               => result['pipeline'],
-        :design_id              => result['design_id'],
-        :cassette               => result['cassette'],
-        :backbone               => result['backbone'],
-        :floxed_exon            => result['floxed_start_exon'],
-        :genbank_files_urls     => {
-          :escell_clone     => "#{conf['attribution_link']}targ_rep/alleles/#{result['allele_id']}/escell-clone-genbank-file",
-          :targeting_vector => "#{conf['attribution_link']}targ_rep/alleles/#{result['allele_id']}/targeting-vector-genbank-file"
-        },
-        :escell_clones          => { :conditionals => {}, :non_conditionals => {} },
-        :targeting_vectors      => {},
-        :intermediate_vectors   => {}
-      })
-    else
-      design_type =
-        case result['mutation_subtype']
+    if data.empty?
+      data.update(
+        'intermediate_vectors' => [],
+        'targeting_vectors'    => [],
+        'conditionals'         => { 'cells' => [] },
+        'non_conditionals'     => { 'cells' => [] }
+      )
+    end
+    
+    design_type =
+      case result['mutation_subtype']
         when 'conditional_ready'        then 'Conditional (Frameshift)'
         when 'deletion'                 then 'Deletion'
-        when 'targeted_non_conditional' then 'Targeted, non-conditional'
+        when 'targeted_non_conditional' then 'Targeted, Non-Conditional'
         else ''
-        end
-      
-      unless data[:targeting_vectors].keys.include? result['targeting_vector']
-        data[:targeting_vectors][result['targeting_vector']] = { :design_type => design_type }
       end
-      
-      unless data[:intermediate_vectors].keys.include? result['intermediate_vector']
-        data[:intermediate_vectors][result['intermediate_vector']] = { :design_type => design_type }
-      end
+    
+    allele_image = "#{conf['attribution_link']}targ_rep/alleles/#{result['allele_id']}/allele-image"
+    genbank_file = "#{conf['attribution_link']}targ_rep/alleles/#{result['allele_id']}/escell-clone-genbank-file"
+    
+    # Intermediate Vectors
+    data['intermediate_vectors'].push(
+      'name'        => result['intermediate_vector'],
+      'design_id'   => result['design_id'],
+      'design_type' => design_type,
+      'floxed_exon' => result['floxed_start_exon']
+    ) unless result['mutation_subtype'] == 'targeted_non_conditional'
+    
+    # Targeting Vectors
+    data['targeting_vectors'].push(
+      'name'         => result['targeting_vector'],
+      'design_id'    => result['design_id'],
+      'design_type'  => design_type,
+      'cassette'     => result['cassette'],
+      'backbone'     => result['backbone'],
+      'floxed_exon'  => result['floxed_start_exon'],
+      'genbank_file' => "#{conf['attribution_link']}targ_rep/alleles/#{result['allele_id']}/targeting-vector-genbank-file"
+    ) unless result['mutation_subtype'] == 'targeted_non_conditional'
+    
+    # Non-Conditionals
+    if result['mutation_subtype'] == 'targeted_non_conditional'
+      data['non_conditionals'].update(
+        'design_type'  => design_type,
+        'allele_image' => allele_image,
+        'genbank_file' => genbank_file
+      )
       
       next if result['escell_clone'].nil? or result['escell_clone'].empty?
       
-      if result['mutation_subtype'] == 'targeted_non_conditional'
-        data[:escell_clones][:non_conditionals].update({
-          result['escell_clone'] => {
-            :allele_symbol_superscript  => result['allele_symbol_superscript'],
-            :parental_cell_line                => result['parental_cell_line'],
-            :targeting_vector           => result['targeting_vector']
-          }
-        })
-        data[:escell_non_cond_img_link] = "#{conf['attribution_link']}targ_rep/alleles/#{result['allele_id']}/allele-image"
-      else
-        data[:escell_clones][:conditionals].update({
-          result['escell_clone'] => {
-            :allele_symbol_superscript  => result['allele_symbol_superscript'],
-            :parental_cell_line         => result['parental_cell_line'],
-            :targeting_vector           => result['targeting_vector']
-          }
-        })
-        data[:escell_cond_img_link] = "#{conf['attribution_link']}targ_rep/alleles/#{result['allele_id']}/allele-image"
-      end
+      data['non_conditionals']['cells'].push(
+        'name'                      => result['escell_clone'],
+        'allele_symbol_superscript' => result['allele_symbol_superscript'],
+        'parental_cell_line'        => result['parental_cell_line'],
+        'targeting_vector'          => result['targeting_vector']
+      )
+    
+    # Conditionals
+    else
+      data['conditionals'].update(
+        'design_type'  => design_type,
+        'allele_image' => allele_image,
+        'genbank_file' => genbank_file
+      )
+      
+      next if result['escell_clone'].nil? or result['escell_clone'].empty?
+      
+      data['conditionals']['cells'].push(
+        'name'                      => result['escell_clone'],
+        'allele_symbol_superscript' => result['allele_symbol_superscript'],
+        'parental_cell_line'        => result['parental_cell_line'],
+        'targeting_vector'          => result['targeting_vector']
+      )
     end
   end
+  
+  unless data.empty?
+    data['intermediate_vectors'].uniq!
+    data['targeting_vectors'].uniq!
+    data['conditionals']['cells'].uniq!
+    data['non_conditionals']['cells'].uniq!
+  end
+  
+  return data
 end
 
-def query_ko_attempts_mart( data, project_id )
-  conf    = JSON.load( File.new("#{File.dirname(__FILE__)}/config/datasets/ikmc-dcc-knockout_attempts/config.json","r") )
-  dataset = Biomart::Dataset.new( conf['url'], { :name => conf['dataset_name'] } )
-  results = dataset.search({
-    :filters => { 'ikmc_project_id' => project_id },
-    :attributes => [ 'status' ],
-    :process_results => true
-  })
-  results.each { |result| data.update({ :status => result['status'] }) }
-end
-
-def query_kermits( data, project_id )
+# Will query Kermits mart
+def get_mice( ensembl_gene_id )
   conf    = JSON.load( File.new("#{File.dirname(__FILE__)}/config/datasets/sanger-kermits/config.json","r") )
   dataset = Biomart::Dataset.new( conf['url'], { :name => conf['dataset_name'] } )
   results = dataset.search({
-    :filters => { 'colony_name' => data[:colony_prefix][0] },
-    :attributes => ['marker_symbol', 'allele_name', 'escell_clone', 'escell_strain', 'escell_line'],
+    :filters => { 'ensembl_gene_id' => ensembl_gene_id },
+    :attributes => ['allele_name', 'escell_clone', 'escell_strain', 'escell_line'],
     :process_results => true
   })
   
-  if results
-    data.update({ :mice => [] })
-    
-    results.each do |result|
-      data[:mice].push({
-        :marker_symbol  => result['marker_symbol'],
-        :allele_name    => result['allele_name'],
-        :escell_clone   => result['escell_clone'],
-        :escell_strain  => result['escell_strain'],
-        :escell_line    => result['escell_line'],
-      })
-    end
+  return { 'mice' => results } unless results.empty?
+  return {}
+end
+
+def order_buttons_url( data )
+  mgi_accession_id  = data['mgi_accession_id'][4..-1]
+  pipeline          = data['pipeline']
+  marker_symbol     = data['marker_symbol']
+  project_id        = data['project_id']
+  
+  if pipeline == "KOMP-CSD"
+    return {
+      'order_vector_url'   => "http://www.komp.org/vectorOrder.php?projectid=#{project_id}",
+      'order_cell_url'     => "http://www.komp.org/orders.php?project=CSD#{project_id}&amp;product=1",
+      'order_default_url'  => "http://www.komp.org/geneinfo.php?project=CSD#{project_id}"
+    }
+  elsif pipeline == "KOMP-Regeneron"
+    return {
+      'order_vector_url'   => "http://www.komp.org/vectorOrder.php?projectid=#{project_id}",
+      'order_cell_url'     => "http://www.komp.org/orders.php?project=#{project_id}&amp;product=1",
+      'order_default_url'  => "http://www.komp.org/geneinfo.php?project=#{project_id}"
+    }
+  elsif pipeline == "EUCOMM" or pipeline == "mirKO"
+    return {
+      'order_vector_url'   => "http://www.eummcr.org/final_vectors.php?mgi_id=#{mgi_accession_id}",
+      'order_cell_url'     => "http://www.eummcr.org/es_cells.php?mgi_id=#{mgi_accession_id}",
+      'order_mouse_url'    => "http://www.emmanet.org/mutant_types.php?keyword=#{marker_symbol}%25EUCOMM&select_by=InternationalStrainName&search=ok",
+      'order_default_url'  => "http://www.eummcr.org/order.php"
+    }
+  elsif pipeline == "NorCOMM"
+    return {
+      'order_default_url'  => "http://www.phenogenomics.ca/services/cmmr/escell_services.html"
+    }
+  else
+    return {}
   end
 end

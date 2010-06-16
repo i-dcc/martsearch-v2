1b0ab169709c5981e0cfe33b908758e137e3a8b0
Daz Oakley <do2@sanger.ac.uk>
2010-06-16 15:08:13 +0100

    Deployment bugfix! :(

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,8 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/ols_database.yml #{release_path}/config/ols_database.yml"
     run "ln -nfs #{shared_path}/pheno_overview.xls #{release_path}/public/pheno_overview.xls"
     run "ln -nfs #{shared_path}/pheno_images #{release_path}/public/images/pheno_images"
+    
+    run "ln -nfs #{release_path}/public #{shared_path}/htdocs/mouseportal"
   end
   
   desc "Set the permissions of the filesystem so that others in the team can deploy"
@@ -29,20 +31,20 @@ namespace :deploy do
     run "chmod 02775 #{release_path}"
   end
   
-  desc "Regenerate the Sanger Phenotyping heatmap upon release."
-  task :generate_heatmap do
-    brave_new_world_env = {
-      :PATH     => "/software/team87/brave_new_world/bin:/software/perl-5.8.8/bin:/usr/local/lsf/7.0/linux2.6-glibc2.3-x86_64/bin:/usr/bin:$PATH",
-      :PERL5LIB => "/software/team87/brave_new_world/lib/perl5:/software/team87/brave_new_world/lib/perl5/x86_64-linux-thread-multi",
-      :CONF_DIR => "/software/team87/brave_new_world/conf",
-      :LOG_DIR  => "/software/team87/brave_new_world/logs/cron"
-    }
-    
-    run "htgt-env.pl --live perl #{release_path}/config/datasets/sanger-phenotyping/generate-spreadsheet.pl", :env => brave_new_world_env
-    run "chgrp team87 #{release_path}/public/pheno_overview.xls"
-    run "chmod g+w #{release_path}/public/pheno_overview.xls"
-  end
+  # desc "Regenerate the Sanger Phenotyping heatmap upon release."
+  # task :generate_heatmap do
+  #   brave_new_world_env = {
+  #     :PATH     => "/software/team87/brave_new_world/bin:/software/perl-5.8.8/bin:/usr/local/lsf/7.0/linux2.6-glibc2.3-x86_64/bin:/usr/bin:$PATH",
+  #     :PERL5LIB => "/software/team87/brave_new_world/lib/perl5:/software/team87/brave_new_world/lib/perl5/x86_64-linux-thread-multi",
+  #     :CONF_DIR => "/software/team87/brave_new_world/conf",
+  #     :LOG_DIR  => "/software/team87/brave_new_world/logs/cron"
+  #   }
+  #   
+  #   run "htgt-env.pl --live perl #{release_path}/config/datasets/sanger-phenotyping/generate-spreadsheet.pl", :env => brave_new_world_env
+  #   run "chgrp team87 #{release_path}/public/pheno_overview.xls"
+  #   run "chmod g+w #{release_path}/public/pheno_overview.xls"
+  # end
 end
 
 after "deploy:update_code", "deploy:symlink_shared"
-after "deploy:symlink", "deploy:generate_heatmap", "deploy:fix_perms"
+after "deploy:symlink", "deploy:fix_perms"

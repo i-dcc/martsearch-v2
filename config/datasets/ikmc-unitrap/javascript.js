740299960793ce634414d18d983490699d0a8040
Daz Oakley <do2@sanger.ac.uk>
2010-07-05 14:18:44 +0100

    Final finishing touches to the refactor. closes #1167

--- config/datasets/ikmc-unitrap/javascript.js
+++ config/datasets/ikmc-unitrap/javascript.js
@@ -8,10 +8,10 @@ jQuery(document).ready(function() {
     var parent = jQuery(this).parentsUntil("div.dataset_content").last().parent();
     
     // Hide any existing tables...
-    parent.find("div.unitraps_by div").hide();
+    parent.find("div.unitraps_by div").slideUp("fast");
     
     // Show the one we want...
-    parent.find("div.unitraps_by div." + jQuery(this).attr("rel")).show();
+    parent.find("div.unitraps_by div." + jQuery(this).attr("rel")).slideDown("fast");
     
     return false;
   });

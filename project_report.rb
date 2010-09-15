0a47a88e08f91a4503e039efff7fb8d1a9cfbb5a
Nelo Onyiah <io1@sanger.ac.uk>
2010-09-15 16:59:57 +0100

    Set each empty qc_metric to '-' or count it [refs #3859]

--- project_report.rb
+++ project_report.rb
@@ -165,6 +165,34 @@ def get_mice( marker_symbol )
   })
   results.reject! { |result| result['status'].nil? }
   
+  # Test for QC data
+  qc_metrics = [
+        'qc_southern_blot',
+        'qc_tv_backbone_assay',
+        'qc_five_prime_lr_pcr',
+        'qc_loa_qpcr',
+        'qc_homozygous_loa_sr_pcr',
+        'qc_neo_count_qpcr',
+        'qc_lacz_sr_pcr',
+        'qc_five_prime_cass_integrity',
+        'qc_neo_sr_pcr',
+        'qc_mutant_specific_sr_pcr',
+        'qc_loxp_confirmation',
+        'qc_three_prime_lr_pcr'
+  ]
+
+  # Set each empty qc_metric to '-' or count it
+  results.each do |result|
+    result['qc_count'] = 0
+    qc_metrics.each do |metric|
+      if result[metric].nil?
+        result[metric] = '-'
+      else
+        result['qc_count'] = result['qc_count'] + 1
+      end
+    end
+  end
+
   results.empty? ? {} : { 'mice' => results }
 end
 

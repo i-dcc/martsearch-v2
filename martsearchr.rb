870347fc343e6137955c329f8be9584acc53cb5e
Daz Oakley <do2@sanger.ac.uk>
2010-09-24 08:52:09 +0100

    Small css/javascript bugfix for the Kermits QC data toggles.

--- martsearchr.rb
+++ martsearchr.rb
@@ -40,7 +40,7 @@ require "#{MARTSEARCHR_PATH}/lib/martsearch.rb"
 # We're going to use the version number as a cache breaker 
 # for the CSS and javascript code. Update with each release 
 # of your portal (especially if you change the CSS or JS)!!!
-PORTAL_VERSION    = "0.0.16"
+PORTAL_VERSION    = "0.0.17"
 DEFAULT_CSS_FILES = [
   "reset.css",
   "jquery.prettyPhoto.css",
